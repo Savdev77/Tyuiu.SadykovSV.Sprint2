36bdee9c2c540e63ecfc2db8f8c74f5c1fc44047
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-17 02:14:53 +0500

    Task6

---
+++ Tyuiu.SadykovSV.Sprint2.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SadykovSV.Sprint2.Task7.V12.Lib
+{
+    public class DataService : ISprint2Task7V12
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool result;
+            if (((y>= 2 - x) && (y<=Math.Pow(x,2))) || ((y <= Math.Pow(x, 2)) && (y <= 2 - x) && (x>=0) && (y>=0)))
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.SadykovSV.Sprint2.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SadykovSV.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.SadykovSV.Sprint2.Task7.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 0.0;
+            bool result = ds.CheckDotInShadedArea(x,y);
+            bool expected = true;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
