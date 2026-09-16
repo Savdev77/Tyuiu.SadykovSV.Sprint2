c636e940c67f18cbad4b954999aa81622e0dc5ff
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-17 02:43:03 +0500

    Task7

--- Tyuiu.SadykovSV.Sprint2.Task7.V12.Lib/DataService.cs
+++ Tyuiu.SadykovSV.Sprint2.Task7.V12.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.SadykovSV.Sprint2.Task7.V12.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool result;
-            if (((y>= 2 - x) && (y<=Math.Pow(x,2))) || ((y <= Math.Pow(x, 2)) && (y <= 2 - x) && (x>=0) && (y>=0)))
+            if (((y>= 2 - x) && (y<=Math.Pow(x,2)) && (x<=0)) || ((y <= Math.Pow(x, 2)) && (y <= 2 - x) && (x>=0) && (y>=0)))
             {
                 result = true;
             }
--- Tyuiu.SadykovSV.Sprint2.Task7.V12/Program.cs
+++ Tyuiu.SadykovSV.Sprint2.Task7.V12/Program.cs
@@ -9,34 +9,31 @@ Console.WriteLine("* Выполнил: Садыков С.В. | ПИНБ-26-1    
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                                *");
 Console.WriteLine("* Написать программу на C#,                                               *");
-Console.WriteLine("* g (год), m (порядковый номер месяца) и n (число).                       *");
-Console.WriteLine("* По заданным g, n и m определить дату следующего дня.                   *");
+Console.WriteLine("* которая запрашивает исходные данные (вещественные значения)             *");
+Console.WriteLine("* и вычисляет, находится ли точка с координатами X,Y                      *");
+Console.WriteLine("* в заштрихованной области.                                               *");
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* Исходные данные:                                                        *");
 Console.WriteLine("***************************************************************************");
 
-Console.WriteLine("Введите число n:");
-int n = int.Parse(Console.ReadLine());
-if ((n < 1) || (n > 31))
-{
-    Console.WriteLine("Ошибка: число n должно быть в диапазоне от 1 до 31.");
-    return;
-}
-Console.WriteLine("Введите номер месяца m:");
-int m = int.Parse(Console.ReadLine());
-if ((m < 1) || (m > 12))
-{
-    Console.WriteLine("Ошибка: номер месяца m должен быть в диапазоне от 1 до 12.");
-    return;
-}
-Console.WriteLine("Введите год g:");
-int g = int.Parse(Console.ReadLine());
-if (g < 1)
-{
-    Console.WriteLine("Ошибка: год g должен быть положительным числом.");
-    return;
-}
+Console.WriteLine("Введите координату x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату y: ");
+double y = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("***************************************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
 Console.WriteLine("***************************************************************************");
+
+DataService ds = new DataService();
+bool res = ds.CheckDotInShadedArea(x, y);
+if (res)
+{
+    Console.WriteLine($"Точка с кординатами ({x};{y}) находится в заштрихованной области!");
+}
+else
+{
+    Console.WriteLine($"Точка с кординатами ({x};{y}) не находится в заштрихованной области!");
+}
+Console.ReadKey();
